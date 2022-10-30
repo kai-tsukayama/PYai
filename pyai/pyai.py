7259250674f4c3712f1a12a51a576b18f53b140a
kai-tsukayama <tsukkai0402@gmail.com>
2022-10-30 17:42:27 +0900

    pyaiのファイル

---
+++ pyai/pyai.py
@@ -0,0 +1,15 @@
+from responder import *
+
+class Pyai:
+  def __init__(self,name):
+    self.name=name
+    self.responder=RandomResponder("Random")
+
+  def dialogue(self,input):
+    return self.responder.response(input)
+
+  def get_responder_name(self):
+    return self.responder.name
+
+  def get_name(self):
+    return self.name
---
+++ pyai/responder.py
@@ -0,0 +1,31 @@
+import random
+
+class Responder:
+  def __init__(self,name):
+    self.name=name
+
+  def response(self,input):
+    return ""
+
+  def get_name(self):
+    return self.name
+
+class RepeatResponder(Responder):
+  def response(self,input):
+    return "{}ってなに？".format(input)
+
+class RandomResponder(Responder):
+  
+  def __init__(self,name):
+    super().__init__(name)
+    self.responses=[]
+    rfile=open("dics/random.txt", "r", encoding="utf_8")
+    r_lines=rfile.readlines()
+    rfile.close()
+    for line in r_lines:
+      str=line.rstrip("\n")
+      if (str!=""):
+        self.responses.append(str)
+
+  def response(self,input):
+      return (random.choice(self.responses))
