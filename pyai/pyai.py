f8f8c7bd3282a1e81a7a1d69185580a8a1d4d5db
kai-tsukayama <tsukkai0402@gmail.com>
2022-11-07 14:55:16 +0900

    PYai20221107

--- pyai/pyai.py
+++ pyai/pyai.py
@@ -1,15 +1,21 @@
 from responder import *
+from dictionary import*
 
 class Pyai:
   def __init__(self,name):
     self.name=name
-    self.responder=RandomResponder("Random")
+    self.dictionary=Dictionary()
+
+    self.res_random=RandomResponder("Random",self.dictionary)
+    self.res_what=RepeatResponder("Repeat?",self.dictionary)
+    self.res_pattern=PatternResponder("Pattern",self.dictionary)
 
   def dialogue(self,input):
+    x=random.randint(0,100)
+    if x<=60:
+      self.responder=self.res_pattern
+    elif 61<=x<=90:
+      self.responder=self.res_random
+    else:
+      self.responder=self.res_what
     return self.responder.response(input)
-
-  def get_responder_name(self):
-    return self.responder.name
-
-  def get_name(self):
-    return self.name
